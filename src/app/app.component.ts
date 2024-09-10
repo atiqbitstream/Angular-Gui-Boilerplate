ed26cc854e5518d754ba692d49eb11ebbc061e67
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-10 23:19:03 +0500

    created topnav , sidenav and main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopnavComponent } from './topnav/topnav.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TopnavComponent,SidenavComponent,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
