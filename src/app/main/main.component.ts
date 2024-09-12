3810162c96681fa95d8b1fef0e507ebee587cc05
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-12 15:18:06 +0500

    fit the 'top-section' components in body horizontally

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FeaturesComponent } from "../top-section/features/features.component";
 import { IssuesComponent } from "../top-section/issues/issues.component";
+import { SummaryComponent } from '../top-section/summary/summary.component';
+import { OverdueComponent } from '../top-section/overdue/overdue.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FeaturesComponent, IssuesComponent],
+  imports: [FeaturesComponent, IssuesComponent,SummaryComponent,OverdueComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
