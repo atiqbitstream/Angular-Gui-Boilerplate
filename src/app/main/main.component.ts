5bab05411e2dd249d1ca64b976994720c2e92566
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-12 12:20:06 +0500

    issues component skeleton done

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FeaturesComponent } from "../top-section/features/features.component";
+import { IssuesComponent } from "../top-section/issues/issues.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FeaturesComponent],
+  imports: [FeaturesComponent, IssuesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
